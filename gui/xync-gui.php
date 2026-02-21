6ddb3db9bc4fe5a60f5534549de36c077af36ad7
tschettervictor <tschetter.victor@gmail.com>
2026-02-20 17:12:19 -0700

    fix config path in gui

--- gui/xync-gui.php
+++ gui/xync-gui.php
@@ -12,7 +12,7 @@ $ext_config_dir = "/var/etc/xync";
 $ext_config = "{$ext_config_dir}/xync.conf.ext";
 $cwdir = exec("/usr/bin/grep 'INSTALL_DIR=' " . escapeshellarg($ext_confif) . " | /usr/bin/cut -d'\"' -f2");
 $app_command = "{$cwdir}/xync-dist/xync.sh";
-$app_config = "{$cwdir}/conf/xync.conf.ext";
+$app_config = "{$cwdir}/conf/xync.conf";
 $log_dir_default = "{$cwdir}/logs";
 $cron_uuid = "68c74f5d-1234-4321-a1b2-c3d4e5f6a7b8"; 
 $checkbox_vars = ['ALLOW_RECONCILIATION', 'ALLOW_ROOT_DATASETS', 'RECURSE_CHILDREN'];
